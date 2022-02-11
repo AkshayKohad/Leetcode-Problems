fb9a2498d2368056847a00c3106de6320e81fc79
AkshayKohad <65406900+AkshayKohad@users.noreply.github.com>
2022-02-11 23:53:26 +0530

    Create Course Schedule II.cpp

---
+++ Graphs/Course Schedule II.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    
+    void solve(int src,vector<int>adj[],vector<int>&visited,vector<int>temp,stack<int>&s,bool &flag)
+    {
+        visited[src] = 1;
+        temp[src] = 1;
+        //cout<<src<<endl;
+        for(auto u : adj[src])
+        {
+            if(visited[u]==0)
+            {
+                solve(u,adj,visited,temp,s,flag);
+            }
+            if(flag==true)
+            {
+                //cout<<"hello"<<endl;
+                return;
+            }
+            
+            if(temp[u]==1)
+            {
+                flag = true;
+             //   cout<<"hello"<<endl;
+                return;
+            }
+        }
+       // cout<<src<<endl;
+        s.push(src);
+        temp[src] = 0;
+        
+        
+    }
+    
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        
+        int n = numCourses;
+        vector<int>adj[n];
+        
+        for(int i=0;i<prerequisites.size();i++)
+        {
+            adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        }
+        vector<int>a;
+        vector<int>visited(n,0);
+        vector<int>temp(n,0);
+        stack<int>s;
+        bool flag=false;
+        for(int i=0;i<n;i++)
+        {
+         if(visited[i]==0)   
+        solve(i,adj,visited,temp,s,flag);
+            
+            if(flag==true)
+            {
+             return a;   
+            }
+        }
+        
+        while(s.empty()==false)
+        {
+            a.push_back(s.top());
+            s.pop();
+        }
+        
+        reverse(a.begin(),a.end());
+        return a;
+    }
+};
