6eefd2bf9aeefd6f7a9181e886bdd77870829709
AkshayKohad <65406900+AkshayKohad@users.noreply.github.com>
2022-06-16 23:30:10 +0530

    Create Single Element in a Sorted Array.cpp

---
+++ Binary Search/Single Element in a Sorted Array.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        
+        
+        int n = nums.size();
+        if(n==1)
+            return nums[0];
+        
+        int l = 0;
+        int h = n-1;
+        
+        while(l<h)
+        {
+         int mid = (l+h)/2;
+            
+            if(mid!=0 && mid!=n-1)
+            {
+                if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])
+                    return nums[mid];
+                
+                else if(nums[mid]==nums[mid+1])
+                {
+                    if((mid+1+1)%2==0)
+                    {
+                        l = mid+1;
+                    }
+                    
+                    else{
+                        h = mid-1;
+                    }
+                }
+                
+                else if(nums[mid]==nums[mid-1])
+                {
+                   if((mid+1)%2==0)
+                   {
+                       l = mid+1;
+                   }
+                    
+                    else{
+                        h = mid-1;
+                    }
+                }
+            }
+            
+            else if(mid==0)
+            {
+                if(nums[mid]!=nums[mid+1])
+                    return nums[mid];
+                
+                else
+                    l = mid+1;
+                
+            }
+            
+            else if(mid==n-1)
+            {
+                if(nums[mid]!=nums[mid-1])
+                    return nums[mid];
+                
+                else
+                    h = mid-1;
+            }
+            
+        }
+        
+        return nums[l];
+    }
+    
+};
