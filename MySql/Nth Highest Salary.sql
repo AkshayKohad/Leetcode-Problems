333776a104966df8f0964b52a7e1fdf85b0b221b
AkshayKohad <65406900+AkshayKohad@users.noreply.github.com>
2022-06-27 23:57:39 +0530

    Update Nth Highest Salary.sql

--- MySql/Nth Highest Salary.sql
+++ MySql/Nth Highest Salary.sql
@@ -15,3 +15,19 @@ BEGIN
       
   );
 END
+
+
+#approach2
+
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+        select min(salary)
+        from 
+            (select distinct salary from employee 
+            order by salary desc
+            limit N) t 
+        having count(*) >= N
+        
+  );
+END
