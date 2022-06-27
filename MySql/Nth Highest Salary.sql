a81ad94fa31a3176e544c906d200cf98b7940872
AkshayKohad <65406900+AkshayKohad@users.noreply.github.com>
2022-06-27 22:17:22 +0530

    Create Nth Highest Salary.sql

---
+++ MySql/Nth Highest Salary.sql
@@ -0,0 +1,17 @@
+
+
+#using concept of limit and offset
+# offset is like starting point from where limit starts the considerations next to offset points
+
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+ SET N = N-1;
+  RETURN (
+      
+      select IFNULL((select distinct salary from Employee
+                       order by salary desc limit N,1
+                    ),NULL) as 'getNthHighestSalary(N+1)'
+      # Write your MySQL query statement below.
+      
+  );
+END
