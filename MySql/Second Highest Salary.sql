19d00256afa7a4de6028e06349c6d6ca41437f23
AkshayKohad <65406900+AkshayKohad@users.noreply.github.com>
2022-06-27 20:46:09 +0530

    Update Second Highest Salary.sql

--- MySql/Second Highest Salary.sql
+++ MySql/Second Highest Salary.sql
@@ -1,7 +1,15 @@
 # Write your MySQL query statement below
 
+
+#approach1
 Select 
     IFNULL((SELECT salary FROM Employee WHERE salary <(SELECT MAX(salary) FROM Employee)
 ORDER BY salary desc limit 1),NULL) as SecondHighestSalary;
+
+#approach2
+
+select 
+    IFNULL((select distinct salary from Employee order by salary desc limit 1,1),NULL) as SecondHighestSalary
+
 
 
