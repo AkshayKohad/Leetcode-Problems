96f91a204af8f4958345f790b5e781e6df4a2463
AkshayKohad <65406900+AkshayKohad@users.noreply.github.com>
2022-07-03 20:58:37 +0530

    Create Flatten Binary Tree to Linked List(Recursive Approach).cpp

---
+++ Tree/Flatten Binary Tree to Linked List(Recursive Approach).cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    
+    TreeNode*prev = NULL;
+    
+    void solve(TreeNode*root)
+    {
+        if(root==NULL)
+            return;
+        
+        solve(root->right);
+        solve(root->left);
+        
+        root->right = prev;
+        root->left = NULL;
+        
+        prev = root;
+    }
+    
+    void flatten(TreeNode* root) {
+        
+        if(root==NULL)
+            return;
+        
+         solve(root);
+        
+    }
+};
