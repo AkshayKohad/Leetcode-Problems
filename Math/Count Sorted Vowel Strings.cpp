324e4169f5ed0c673b11a9d38e62201b007709c4
AkshayKohad <65406900+AkshayKohad@users.noreply.github.com>
2023-03-23 00:05:44 +0530

    Create Count Sorted Vowel Strings.cpp

---
+++ Math/Count Sorted Vowel Strings.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+   
+    int countVowelStrings(int n) {
+        
+        int  a,e,i,o,u;
+         a = e = i = o = u = 1;
+
+       if(n==1)
+       return a+e+i+o+u;
+
+        n = n-1;
+       while(n--)
+       {
+           o += u;
+           i += o;
+           e += i;
+           a += e;
+       }
+
+       return a+e+i+o+u;
+
+
+    }
+};
