04cd586aa6506e2fdb927dbc5a16cacb0b79edc1
AkshayKohad <65406900+AkshayKohad@users.noreply.github.com>
2024-11-22 05:43:08 +0530

    Create Two Sum III - Data structure design.cpp

---
+++ Design Questions/Two Sum III - Data structure design.cpp
@@ -0,0 +1,63 @@
+Design a data structure that accepts a stream of integers and checks if it has a pair of integers that sum up to a particular value.
+
+Implement the TwoSum class:
+
+TwoSum() Initializes the TwoSum object, with an empty array initially.
+void add(int number) Adds number to the data structure.
+boolean find(int value) Returns true if there exists any pair of numbers whose sum is equal to value, otherwise, it returns false.
+ 
+
+Example 1:
+
+Input
+["TwoSum", "add", "add", "add", "find", "find"]
+[[], [1], [3], [5], [4], [7]]
+Output
+[null, null, null, null, true, false]
+
+Explanation
+TwoSum twoSum = new TwoSum();
+twoSum.add(1);   // [] --> [1]
+twoSum.add(3);   // [1] --> [1,3]
+twoSum.add(5);   // [1,3] --> [1,3,5]
+twoSum.find(4);  // 1 + 3 = 4, return true
+twoSum.find(7);  // No two integers sum up to 7, return false
+ 
+
+Constraints:
+
+-105 <= number <= 105
+-231 <= value <= 231 - 1
+At most 104 calls will be made to add and find.
+
+
+
+class TwoSum {
+public:
+    unordered_map<int,int>cnt_freq;
+
+    TwoSum() {
+    }
+    
+    void add(int number) {
+        cnt_freq[number]++;
+    }
+    
+    bool find(int value) {
+        for(auto k:cnt_freq){
+            long val = k.first;
+            long val_freq = k.second;
+            long remaining_val = value-val;
+    
+            if((remaining_val != val && cnt_freq.count(remaining_val)>0)  || (remaining_val == val && val_freq>1))return true;
+        }
+        return false;
+    }
+};
+
+/**
+ * Your TwoSum object will be instantiated and called as such:
+ * TwoSum* obj = new TwoSum();
+ * obj->add(number);
+ * bool param_2 = obj->find(value);
+ */
