3b46bacae40a235306a6c4129adeab3b57f59b49
AkshayKohad <65406900+AkshayKohad@users.noreply.github.com>
2026-06-15 00:09:04 +0530

    Update Single Element in a Sorted Array.cpp

--- Binary Search/Single Element in a Sorted Array.cpp
+++ Binary Search/Single Element in a Sorted Array.cpp
@@ -1,71 +1,33 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        
-        
         int n = nums.size();
-        if(n==1)
-            return nums[0];
-        
-        int l = 0;
-        int h = n-1;
-        
-        while(l<h)
-        {
-         int mid = (l+h)/2;
-            
-            if(mid!=0 && mid!=n-1)
-            {
-                if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])
-                    return nums[mid];
-                
-                else if(nums[mid]==nums[mid+1])
-                {
-                    if((mid+1+1)%2==0)
-                    {
-                        l = mid+1;
-                    }
-                    
-                    else{
-                        h = mid-1;
-                    }
-                }
-                
-                else if(nums[mid]==nums[mid-1])
-                {
-                   if((mid+1)%2==0)
-                   {
-                       l = mid+1;
-                   }
-                    
-                    else{
-                        h = mid-1;
-                    }
-                }
-            }
-            
-            else if(mid==0)
-            {
-                if(nums[mid]!=nums[mid+1])
-                    return nums[mid];
-                
-                else
-                    l = mid+1;
-                
-            }
-            
-            else if(mid==n-1)
-            {
-                if(nums[mid]!=nums[mid-1])
-                    return nums[mid];
-                
-                else
-                    h = mid-1;
-            }
-            
-        }
-        
-        return nums[l];
-    }
+    if(n==1)return nums[0];
+    // Replace this placeholder return statement with your code
+    int l = 0;
+    int r = n-1;
     
+    while(l<=r){
+        int mid = (r-l)/2 + l;
+        
+        if(mid == 0){
+            if(nums[mid]!=nums[mid+1])return nums[mid];
+            else l = mid+1;
+        }else if(mid == n-1){
+            if(nums[mid]!=nums[mid-1])return nums[mid];
+            else r = mid-1;
+        }else{
+            if(nums[mid] == nums[mid+1]){
+                if(mid%2 == 1)r = mid-1;
+                else l = mid+1;
+            }else if(nums[mid] == nums[mid-1]){
+                if(mid%2 == 1)l = mid+1;
+                else r = mid-1;
+            }else{
+                return nums[mid];
+            }
+        }
+    }
+    return -1;
+    }
 };
